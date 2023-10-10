8cf554a14d8aaf13dd2ac16e94071efa8c4f31a5
Parker Johnson <johnsonpar@byui.edu>
2023-10-10 10:23:17 -0600

    add menu, players, and computer

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+let numPlayers = 2;
+let player1Name = "Player 1";
+let player2Name = "Player 2";
+let player1PaddleColor = "#00ff00";
+let player2PaddleColor = "#ff0000";
+let paused = false;
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -29,7 +37,7 @@ let paddleA = {
   score: 0
 };
 let paddleB = {
-  x: canvas.width - 20,
+  x: canvas.width - paddleWidth - 10,
   y: canvas.height / 2 - paddleHeight / 2,
   dy: 0,
   score: 0
@@ -43,10 +51,10 @@ function drawBall() {
   ctx.closePath();
 }
 
-function drawPaddle(x, y) {
+function drawPaddle(x, y, color) {
   ctx.beginPath();
   ctx.rect(x, y, paddleWidth, paddleHeight);
-  ctx.fillStyle = 'limegreen';
+  ctx.fillStyle = color;
   ctx.fill();
   ctx.closePath();
 }
@@ -62,17 +70,13 @@ function drawNet() {
 }
 
 function updateObjects() {
-  // update ball position
   ball.x += ball.dx;
   ball.y += ball.dy;
 
-  // detect collisions with walls
   if (ball.y - ballRadius < 0 || ball.y + ballRadius > canvas.height) {
     ball.dy = -ball.dy;
-
   }
 
-  // detect collisions with paddles
   if (ball.dx < 0 && ball.x - ballRadius <= paddleA.x + paddleWidth &&
       ball.y + ballRadius >= paddleA.y &&
       ball.y - ballRadius <= paddleA.y + paddleHeight) {
@@ -88,7 +92,6 @@ function updateObjects() {
     collisionSound.play();
   }
   
-  // update paddle scores and reset ball
   if (ball.x - ballRadius < 0) {
     paddleB.score++;
     pointSound.play();
@@ -103,11 +106,12 @@ function updateObjects() {
     ball.dx = -ball.dx;
   }
 
-  // update paddle positions
+  if (numPlayers === 1) {
+    movePaddleBWithAI();
+  }
   paddleA.y += paddleA.dy;
   paddleB.y += paddleB.dy;
 
-  // restrict paddle positions to canvas boundaries
   if (paddleA.y < 0) {
     paddleA.y = 0;
   }
@@ -125,25 +129,26 @@ function updateObjects() {
 function drawScore() {
   ctx.font = "40px Arial";
   ctx.fillStyle = "white";
-  ctx.fillText(paddleA.score, 200, 50);
-  ctx.fillText(paddleB.score, canvas.width - 200, 50);
+  ctx.fillText(player1Name + ": " + paddleA.score, 20, 50);
+  ctx.fillText(player2Name + ": " + paddleB.score, canvas.width - 300, 50);
 }
 
 function drawFrame() {
-  // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // draw objects
+  if (paused) {
+    ctx.font = "60px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Paused", canvas.width/2-100, canvas.height/2);  
+    requestAnimationFrame(drawFrame);
+    return;
+  }
   drawNet();
-  drawPaddle(paddleA.x, paddleA.y);
-  drawPaddle(paddleB.x, paddleB.y);
+  drawPaddle(paddleA.x, paddleA.y, player1PaddleColor);
+  drawPaddle(paddleB.x, paddleB.y, player2PaddleColor);
   drawBall();
   drawScore();
 
-  // update object positions
   updateObjects();
-
-  // request next frame
   requestAnimationFrame(drawFrame);
 }
 
@@ -160,6 +165,9 @@ function handleKeyDown(event) {
       break;
     case 'ArrowDown':
       paddleB.dy = paddleSpeed;
+      break;
+    case 'Space':
+      paused = !paused;
       break;
   }
 }
@@ -181,9 +189,31 @@ function handleKeyUp(event) {
   }
 }
 
-// add event listeners
+function movePaddleBWithAI() {
+  if (paddleB.y + paddleHeight/2 < ball.y) {
+    paddleB.dy = paddleSpeed;
+  } else if (paddleB.y + paddleHeight/2 > ball.y) {
+    paddleB.dy = -paddleSpeed;
+  } else {
+    paddleB.dy = 0;
+  }
+}
+
+function handlePlayer2ColorChange(event) {
+  player2PaddleColor = event.target.value;
+}
+
+document.getElementById("start-game").addEventListener("click", function() {
+  numPlayers = parseInt(document.getElementById("num-players").value);
+  player1Name = document.getElementById("player1-name").value || "Player 1";
+  player1PaddleColor = document.getElementById("player1-color").value;
+  player2Name = document.getElementById("player2-name").value || "Player 2";
+  player2PaddleColor = document.getElementById("player2-color").value;
+  document.getElementById("menu").style.display = "none";
+  requestAnimationFrame(drawFrame);
+});
+
+document.getElementById("player2-color").addEventListener("input", handlePlayer2ColorChange);
+
 window.addEventListener('keydown', handleKeyDown);
 window.addEventListener('keyup', handleKeyUp);
-
-// start the game loop
-requestAnimationFrame(drawFrame);
